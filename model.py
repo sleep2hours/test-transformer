77cb9d1db6716c792914af8c33e8607f8a0d694d
sleep2hours <1569171127@qq.com>
2022-03-30 16:30:40 +0800

    完成了模型，没有写训练部分

--- model.py
+++ model.py
@@ -7,6 +7,35 @@ from torch.autograd import Variable
 import matplotlib.pyplot as plt
 
 
+class EncoderDecoder(nn.Module):
+    def __init__(self,encoder,decoder,src_embed,tgt_embed,generator):
+        super(EncoderDecoder, self).__init__()
+        self.encoder=encoder
+        self.decoder=decoder
+        self.src_embed=src_embed
+        self.tgt_embed=tgt_embed
+        self.generator=generator
+
+    def forward(self,src,tgt,src_mask,tgt_mask):
+        return self.decode(self.encode(src,src_mask),src_mask,tgt,tgt_mask)
+
+    def encode(self,src,src_mask):
+        return self.encoder(self.src_embed(src),src_mask)
+
+    def decode(self,memory,src_mask,tgt,tgt_mask):
+        return self.decoder(self.tgt_embed(tgt),memory,src_mask,tgt_mask)
+
+class Generator(nn.Module):
+    def __init__(self,d_model,vocab):
+        super(Generator, self).__init__()
+        self.proj=nn.Linear(d_model,vocab)
+
+    def forward(self,x):
+        return F.log_softmax(self.proj(x),dim=-1)
+
+
+
+
 def clones(module,N):
     #把一个网络层拷贝N份
     return nn.ModuleList([copy.deepcopy(module)for i in range(N)])
@@ -117,6 +146,63 @@ class MultiHeadAttention(nn.Module):
         x,self.attn=attention(query,key,value,mask,dropout=self.dropout)
         x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
         return self.linears[-1](x)
+
+class FeedForward(nn.Module):
+    def __init__(self,d_model,d_ff,dropout=0.1):
+        super(FeedForward, self).__init__()
+        self.w_1=nn.Linear(d_model,d_ff)
+        self.w_2=nn.Linear(d_ff,d_model)
+        self.dropout=nn.Dropout(dropout)
+
+    def forward(self,x):
+        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+
+class Embeddings(nn.Module):
+    def __init__(self, d_model, vocab):
+        super(Embeddings, self).__init__()
+        self.lut = nn.Embedding(vocab, d_model)
+        self.d_model = d_model
+
+    def forward(self, x):
+        return self.lut(x) * math.sqrt(self.d_model)
+
+class PositionalEncoding(nn.Module):
+    def __init__(self,d_model,dropout,max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout=nn.Dropout(dropout)
+
+        #计算位置编码
+        pos=torch.zeros(max_len,d_model)
+        position=torch.arange(0,max_len).unsqueeze(1)
+        div_term=torch.exp(-math.log(10000)*torch.arange(0,d_model,2)/d_model)
+        pos[:,0::2]=torch.sin(position*div_term)
+        pos[:,1::2]=torch.cos(position*div_term)
+        pos=pos.unsqueeze(0)
+        self.register_buffer('pos',pos)
+
+    def forward(self,x):
+        x=x+Variable(self.pos[:,:x.size(1),:x.size(2)],requires_grad=False)
+        return self.dropout(x)
+
+
+def make_model(src_vocab,tgt_vocab,N=6,d_model=512,d_ff=2048,h=8,dropout=0.1):
+    c=copy.deepcopy
+    attn=MultiHeadAttention(h,d_model)
+    ff=FeedForward(d_model,d_ff,dropout)
+    position=PositionalEncoding(d_model,dropout)
+    model=EncoderDecoder(
+        Encoder(EncoderLayers(d_model,c(attn),c(ff),dropout),N),
+        Decoder(DecoderLayers(d_model,c(attn),c(attn),c(ff),dropout),N),
+        nn.Sequential(Embeddings(d_model, src_vocab), c(position)),
+        nn.Sequential(Embeddings(d_model, tgt_vocab), c(position)),
+        Generator(d_model, tgt_vocab)
+    )
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform(p)
+    return model
+
+
 
 
 
